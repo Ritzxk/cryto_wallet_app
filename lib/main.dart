5a34b1385ac160a2cfef522f7f3f66b9a23d4ef1
Ritzxk <ritzap449@gmail.com>
2023-02-22 19:05:08 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,21 @@
+import 'package:crypto_wallet/ui/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'ui/authentication.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(),
+      title: 'Crypto Wallet',
+      home: Authentication(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Container());   
-  }
-}
